8f8da6666e6ca2160402897a5f49ac442e0e4f5b
nashta <894963670@qq.com>
2018-01-02 18:12:29 +0800

    更新1：22

--- src/main/java/com/service/pre/ItripHotelOrderServiceImpl.java
+++ src/main/java/com/service/pre/ItripHotelOrderServiceImpl.java
@@ -17,22 +17,22 @@ public class ItripHotelOrderServiceImpl implements ItripHotelOrderService {
 
     @Autowired
     private ItripHotelOrderMapper itripHotelOrderMapper;
-    @Override
+
     public List<ItripHotelOrder> listAllOrderByUserId(Integer userId) {
         return itripHotelOrderMapper.listAllOrderByUserId(userId);
     }
 
-    @Override
+
     public List<ItripHotelOrder> listOradeBySelect(Integer orderStatus, Integer orderNo, Integer orderType, Date creationDate, String linkUserName) {
         return itripHotelOrderMapper.listOradeBySelect(orderStatus, orderNo, orderType, creationDate, linkUserName);
     }
 
-    @Override
+
     public ItripHotelOrder queryOradeById(Integer orderId) {
         return itripHotelOrderMapper.queryOradeById(orderId);
     }
 
-    @Override
+
     public Integer delOrder(Integer order) {
         return itripHotelOrderMapper.delOrder(order);
     }
