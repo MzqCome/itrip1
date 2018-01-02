60abb0b606b4992a57fa0d31571f8000d1e87eb5
MzqCome <1498727170@qq.com>
2018-01-02 19:01:38 +0800

    19:00

--- src/main/java/com/dao/HotelMapper.java
+++ src/main/java/com/dao/HotelMapper.java
@@ -9,6 +9,8 @@ import java.util.List;
  * Created by dell on 2018/1/2.
  */
 public interface HotelMapper {
-    //查询所有酒店
+    //通過酒店名稱查询所有酒店
     public List<ItripHotel> queryAllByHotelName(@Param("hotelName") String hotelName);
+    //通過酒店地址查询所有酒店
+    public List<ItripHotel> queryAllByAddress(@Param("queryAllByAddress") String queryAllByAddress);
 }
